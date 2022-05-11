710289d9b37f24884304ff006e2f818e85aa1590
M-110 <mcleancolinm@gmail.com>
2022-05-11 16:01:20 -0500

    Add pagination and views

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -22,11 +28,21 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
+
     class Meta:
         ordering = '-publish',
+
 
 # Create your models here.
