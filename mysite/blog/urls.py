b5206993fd516b1937528924977244586665375a
M-110 <mcleancolinm@gmail.com>
2022-05-26 19:50:02 -0500

    Add urls

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,7 +2,7 @@
 from . import views
 
 app_name = 'blog'
-urlpatterns= [
+urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
