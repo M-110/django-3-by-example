c9a15c6ebae9a392f918d1ee0e80080d43c04304
M-110 <mcleancolinm@gmail.com>
2022-06-08 16:11:01 -0500

    feat: add share post

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,9 @@
+from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -34,3 +36,21 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   dict(post=post))
+
+
+def post_share(request, post_id):
+    sent = False
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}!\n\n"
+            send_mail(subject, message, 'admin@blog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html',
+                  dict(post=post, form=form, sent=sent))
