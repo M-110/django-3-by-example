91febd579307702b22ed9091bbf683ce63f767f9
M-110 <mcleancolinm@gmail.com>
2022-06-14 16:21:27 -0500

    feat: add login page

---
+++ bookmarks/account/forms.py
@@ -0,0 +1,6 @@
+﻿from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
---
+++ bookmarks/account/urls.py
@@ -0,0 +1,7 @@
+﻿from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('login/', views.user_login, name='login'),
+]
